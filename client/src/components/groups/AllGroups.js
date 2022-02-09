590b848db8e84ab456f06fb442ed3dc1856ba56f
ExodusSoft <97240285+ExodusSoft@users.noreply.github.com>
2022-02-09 02:42:30 +0200

    reinit with admin stuff -- ref:main WnO-W

---
+++ client/src/components/groups/AllGroups.js
@@ -0,0 +1,102 @@
+import React from 'react'
+
+const AllGroups = () => {
+
+    return (
+        <div class="all_groups">
+            <div class="create_group">
+                {/* <img src="https://bootdey.com/img/Content/avatar/avatar1.png" class="single-create-story-bg" /> */}
+                    <div class="story-author">
+                        <i class="fa fa-plus-circle fa-2x text-info"></i>
+                        <p>Create New Group</p>
+                    </div>
+            </div>
+            <div class="single_group">
+                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="single-story-bg" />
+                    <div class="story-author">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" />
+                            <p>Bird Lovers</p>
+                    </div>
+            </div>
+            <div class="single_group">
+                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="single-story-bg" />
+                    <div class="story-author">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" />
+                            <p>Bird Lovers</p>
+                    </div>
+            </div>
+            <div class="single_group">
+                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="single-story-bg" />
+                    <div class="story-author">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" />
+                            <p>Bird Lovers</p>
+                    </div>
+            </div>
+            <div class="single_group">
+                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="single-story-bg" />
+                    <div class="story-author">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" />
+                            <p>Bird Lovers</p>
+                    </div>
+            </div>
+            <div class="single_group">
+                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="single-story-bg" />
+                    <div class="story-author">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" />
+                            <p>Bird Lovers</p>
+                    </div>
+            </div>
+            <div class="single_group">
+                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="single-story-bg" />
+                    <div class="story-author">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" />
+                            <p>Bird Lovers</p>
+                    </div>
+            </div>
+            <div class="single_group">
+                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="single-story-bg" />
+                    <div class="story-author">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" />
+                            <p>Bird Lovers</p>
+                    </div>
+            </div>
+            <div class="single_group">
+                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="single-story-bg" />
+                    <div class="story-author">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" />
+                            <p>Bird Lovers</p>
+                    </div>
+            </div>
+            <div class="single_group">
+                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="single-story-bg" />
+                    <div class="story-author">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" />
+                            <p>Bird Lovers</p>
+                    </div>
+            </div>
+            <div class="single_group">
+                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="single-story-bg" />
+                    <div class="story-author">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" />
+                            <p>Bird Lovers</p>
+                    </div>
+            </div>
+            <div class="single_group">
+                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="single-story-bg" />
+                    <div class="story-author">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" />
+                            <p>Bird Lovers</p>
+                    </div>
+            </div>
+            <div class="single_group">
+                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="single-story-bg" />
+                    <div class="story-author">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" />
+                            <p>Bird Lovers</p>
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export default AllGroups
---
+++ client/src/pages/groups.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react'
+
+import AllGroups from '../components/groups/AllGroups'
+import Posts from '../components/home/Posts'
+import RightSideBarGroup from '../components/home/RightSideBarGroup'
+
+import { useSelector } from 'react-redux'
+import LoadIcon from '../images/loading.gif'
+
+
+let scroll = 0;
+
+const Home = () => {
+    const { homePosts } = useSelector(state => state)
+
+    window.addEventListener('scroll', () => {
+        if(window.location.pathname === '/'){
+            scroll = window.pageYOffset
+            return scroll;
+        }
+    })
+
+    useEffect(() => {
+        setTimeout(() => {
+            window.scrollTo({top: scroll, behavior: 'smooth'})
+        }, 100)
+    },[])
+
+    return (
+        <div className="home row mx-0 my-4">
+            <div className="col-md-8">
+                <AllGroups />
+
+                {
+                    homePosts.loading 
+                    ? <img src={LoadIcon} alt="loading" className="d-block mx-auto" />
+                    : (homePosts.result === 0 && homePosts.posts.length === 0)
+                        ? <h5 className="text-center container my-4">There's no activity from your groups. 
+                            Find some cool groups to joing and the activity will show here</h5>
+                        : <Posts />
+                }
+                
+            </div>
+            
+            <div className="col-md-4">
+                <RightSideBarGroup />
+            </div>
+        </div>
+    )
+}
+
+export default Home
